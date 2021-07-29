89a48eb6ac06324ccc6485d19194595e6d2b91f0
Tayyab <m.tayyab@otif-solutions.com>
2021-07-29 17:20:57 +0500

    Permissions Upsert Replaced with UpdateOrCreate

--- src/Commands/RefreshMenuPermissions.php
+++ src/Commands/RefreshMenuPermissions.php
@@ -69,13 +69,10 @@ class RefreshMenuPermissions extends Command
         $menuItems = MenuItem::all();
         foreach($menuItems as $menuItem){
             if ($menuItem['generate_permission'] == 'ALL'){
-                Permission::upsert([
-                        ['name' => 'create'.str_replace('/','_',strtolower($menuItem['route'])), 'permission_type_id' => $this->cPermission['id'], 'menu_item_id' => $menuItem['id']],
-                        ['name' => 'read'.str_replace('/','_',strtolower($menuItem['route'])), 'permission_type_id' => $this->rPermission['id'], 'menu_item_id' => $menuItem['id']],
-                        ['name' => 'update'.str_replace('/','_',strtolower($menuItem['route'])), 'permission_type_id' => $this->uPermission['id'], 'menu_item_id' => $menuItem['id']],
-                        ['name' => 'delete'.str_replace('/','_',strtolower($menuItem['route'])), 'permission_type_id' => $this->dPermission['id'], 'menu_item_id' => $menuItem['id']],
-                    ],['permission_type_id','menu_item_id'],['name']
-                );
+                Permission::updateOrCreate(['permission_type_id' => $this->cPermission['id'], 'menu_item_id' => $menuItem['id']],['name' => 'create'.str_replace('/','_',strtolower($menuItem['route']))]);
+                Permission::updateOrCreate(['permission_type_id' => $this->rPermission['id'], 'menu_item_id' => $menuItem['id']],['name' => 'read'.str_replace('/','_',strtolower($menuItem['route']))]);
+                Permission::updateOrCreate(['permission_type_id' => $this->uPermission['id'], 'menu_item_id' => $menuItem['id']],['name' => 'update'.str_replace('/','_',strtolower($menuItem['route']))]);
+                Permission::updateOrCreate(['permission_type_id' => $this->dPermission['id'], 'menu_item_id' => $menuItem['id']],['name' => 'delete'.str_replace('/','_',strtolower($menuItem['route']))]);
             }
             elseif ($menuItem['generate_permission'] == 'MANAGE_ONLY')
                 Permission::updateOrCreate(['permission_type_id' => $this->mPermission['id'], 'menu_item_id' => $menuItem['id']],['name' => 'manage'.str_replace('/','_',strtolower($menuItem['route']))]);
